896b868b481789a310769913e9b867551db289bc
MohamedAhmedIbrahem <mohamed1989877@gmail.com>
2021-05-31 13:04:03 +0200

    utils

--- utils.h
+++ utils.h
@@ -1,6 +1,9 @@
 #ifndef __UTILS_H_
 #define __UTILS_H_
+
 #include<string>
+#include<map>
+
 using namespace std;
 
 
@@ -15,21 +18,27 @@ struct Token {
 };
 
 enum operations {
-    ASSIGN = 100,       
+    ASSIGN = 100,    
+
+    //Arithmetic   
     ADD,                
     U_PLUS,             
     SUB,                
     U_MINUS,            
     MUL,                
     DIV,                
-    MOD,                
+    MOD,   
+
+    //bitwise             
     AND,                
     OR,                 
     XOR,                
-    NOT,                
+    NOT,     
+
+    //logical           
     LOGICAL_AND,        
     LOGICAL_OR,         
-    LOGICAL_NOT,      
+    LOGICAL_NOT,    
     GT,            
     GTE,      
     LT,               
@@ -37,6 +46,7 @@ enum operations {
     EQ,              
     NEQ,          
 
+    //quads
 	PUSH,				
 	POP, 				
 	JMP,				
@@ -79,10 +89,48 @@ union value {
     float floatVal;
 };
 
-struct Utils {
 
-    
-    static string replaceTabsWithSpaces(const string& str) {
+class Utils {
+    private:
+    map<int, pair<string, string>> mapper;
+    map<int, pair<string, string>> dTypes;
+    public:
+    Utils(){
+        mapper[ASSIGN]      = {"=", ""};
+        mapper[ADD]         = {"+", "ADD_"};
+        mapper[SUB]         = {"-", "SUB_"};
+        mapper[MUL]         = {"*", "MUL_"};
+        mapper[DIV]         = {"/", "DIV_"};
+        mapper[MOD]         = {"%", "MOD_"};
+        mapper[AND]         = {"&", "AND_"};
+        mapper[OR]          = {"|", "OR_"};
+        mapper[XOR]         = {"^", "XOR_"};
+        mapper[NOT]         = {"~", "NOT_"};
+        mapper[LOGICAL_AND] = {"&&", "AND_"};
+        mapper[LOGICAL_OR]  = {"||", "OR_"};
+        mapper[LOGICAL_NOT] = {"!", "NOT_"};
+        mapper[GT]          = {">", "GT_"};
+        mapper[GTE]         = {">=", "GTE_"};
+        mapper[LT]          = {"<", "LT_"};
+        mapper[LTE]         = {"<=", "LTE_"};
+        mapper[EQ]          = {"==", "EQU_"};
+        mapper[NEQ]         = {"!=", "NEQ_"};
+        mapper[U_PLUS]      = {"+", ""};
+        mapper[U_MINUS]     = {"-", "NEG_"};
+        mapper[PUSH]        = {"", "PUSH_"};
+        mapper[POP]         = {"", "POP_"};
+        mapper[JMP]         = {"", "JMP_"};
+        mapper[JNZ]         = {"", "JNZ_"};
+        mapper[JZ]          = {"", "JZ_"};
+
+        dTypes[VOID]  = {"void", "VOID"};
+        dTypes[BOOL]  = {"bool", "BOOL"};
+        dTypes[CHAR]  = {"char", "CHR"};
+        dTypes[INT]   = {"int", "INT"};
+        dTypes[FLOAT] = {"float", "FLOAT"};
+        dTypes[ERROR] = {"type error", "TYPERR"};
+    }
+    string replaceTabsWithSpaces(const string& str) {
         string ret;
         for (int i = 0; i < str.size(); ++i) {
             if (str[i] == '\t') {
@@ -94,202 +142,51 @@ struct Utils {
         return ret;
     }
 
-    
-    static bool isArithmetic(operations opr) {
-
+    bool isArithmetic(operations opr) {
         return opr>= ADD && opr<=MOD;
-        
     }
 
-    
-    static bool isLogical(operations opr) {
-        return opr >= LOGICAL_AND && opr <= LOGICAL_OR;
-        
+    bool isLogical(operations opr) {
+        return opr >= LOGICAL_AND && opr <= NEQ;
     }
 
-  
-    static bool isBitwiseOpr(operations opr) {
-
+    bool isBitwise(operations opr) {
         return opr >= AND && opr<= NOT;
-        
     }
 
-    
-    static bool isLvalue(operations opr) {
+    bool isAssign(operations opr) {
         return opr == ASSIGN;
     }
 
-   
-    static string oprToString(operations opr) {
-        switch (opr) {
-            case ASSIGN:
-                return "=";
-            case ADD:
-            case U_PLUS:
-                return "+";
-            case SUB:
-            case U_MINUS:
-                return "-";
-            case MUL:
-                return "*";
-            case DIV:
-                return "/";
-            case MOD:
-                return "%";
-            case AND:
-                return "&";
-            case OR:
-                return "|";
-            case XOR:
-                return "^";
-            case NOT:
-                return "~";
-            case LOGICAL_AND:
-                return "&&";
-            case LOGICAL_OR:
-                return "||";
-            case LOGICAL_NOT:
-                return "!";
-            case GT:
-                return ">";
-            case GTE:
-                return ">=";
-            case LT:
-                return "<";
-            case LTE:
-                return "<=";
-            case EQ:
-                return "==";
-            case NEQ:
-                return "!=";
-        }
-
+    string oprToString(operations opr) {
+        if(mapper.count(opr) && mapper[opr].first != "")
+            return mapper[opr].first;
         return "#";
     }
 
-   
-    static string operationToQuadrables(operations opr, dataTypes type = UNKNOWN) {
-		switch (opr) {
-            case ADD:
-                return "ADD_" + dtypeToQuad(type);
-            case SUB:
-				return "SUB_" + dtypeToQuad(type);
-            case MUL:
-                return "MUL_" + dtypeToQuad(type);
-            case DIV:
-                return "DIV_" + dtypeToQuad(type);
-            case MOD:
-                return "MOD_" + dtypeToQuad(type);
-            case AND:
-			case LOGICAL_AND:
-                return "AND_" + dtypeToQuad(type);
-            case OR:
-			case LOGICAL_OR:
-                return "OR_" + dtypeToQuad(type);
-            case XOR:
-                return "XOR_" + dtypeToQuad(type);
-            case NOT:
-			case LOGICAL_NOT:
-                return "NOT_" + dtypeToQuad(type);
-            case GT:
-                return "GT_" + dtypeToQuad(type);
-            case GTE:
-                return "GTE_" + dtypeToQuad(type);
-            case LT:
-                return "LT_" + dtypeToQuad(type);
-            case LTE:
-                return "LTE_" + dtypeToQuad(type);
-            case EQ:
-                return "EQU_" + dtypeToQuad(type);
-            case NEQ:
-                return "NEQ_" + dtypeToQuad(type);
-			case U_MINUS:
-                return "NEG_" + dtypeToQuad(type);
-			case PUSH:
-				return "PUSH_" + dtypeToQuad(type);
-			case POP:
-				return "POP_" + dtypeToQuad(type);
-			case JMP:
-				return "JMP";
-            case JNZ:
-				return "JNZ_" + dtypeToQuad(type);
-			case JZ:
-				return "JZ_" + dtypeToQuad(type);
-        }
-
+    string operationToQuadrables(operations opr, dataTypes type = UNKNOWN) {
+		if(mapper.count(opr) && mapper[opr].second != "")
+            return mapper[opr].second + dtypeToQuad(type);
         return "#";
     }
-
    
-    static bool isIntegerType(dataTypes type) {
-        return (type == DTYPE_BOOL || type == DTYPE_CHAR || type == DTYPE_INT);
+    bool isValidInt(dataTypes type) {
+        return (type >= BOOL && type <= INT);
     }
 
-    /**
-     * Converts the given data type into its corresponding token string.
-     *
-     * @param type the type to convert.
-     *
-     * @return the corresponding token string.
-     */
-    static string dtypeToStr(DataType type) {
-        switch (type) {
-            case DTYPE_VOID:
-                return "void";
-            case DTYPE_BOOL:
-                return "bool";
-            case DTYPE_CHAR:
-                return "char";
-            case DTYPE_INT:
-                return "int";
-            case DTYPE_FLOAT:
-                return "float";
-            case DTYPE_FUNC_PTR:
-                return "pointer to function";
-            case DTYPE_ERROR:
-                return "type error";
-        }
-
+    string dtypeToStr(dataTypes type) {
+        if(dTypes.count(type))
+            return dTypes[type].first;
         return "unknown";
     }
 
-    /**
-     * Converts the given data type into its corresponding quadruple string.
-     *
-     * @param type the type to convert.
-     *
-     * @return the corresponding quadruple string.
-     */
-    static string dtypeToQuad(DataType type) {
-        switch (type) {
-            case DTYPE_VOID:
-                return "VOID";
-            case DTYPE_BOOL:
-                return "BOOL";
-            case DTYPE_CHAR:
-                return "CHR";
-            case DTYPE_INT:
-                return "INT";
-            case DTYPE_FLOAT:
-                return "FLOAT";
-            case DTYPE_FUNC_PTR:
-                return "FNCPTR";
-            case DTYPE_ERROR:
-                return "TYPERR";
-        }
-
+    string dtypeToQuad(dataTypes type) {
+        if(dTypes.count(type))
+            return dTypes[type].second;
         return "unknown";
     }
 
-    /**
-     * Convert data type from t1 into t2.
-     *
-     * @param t1 the type to convert from.
-     * @param t2 the type to convert to.
-     *
-     * @return the corresponding quadruple string.
-     */
-    static string dtypeConvQuad(DataType t1, DataType t2) {
+    string dtypeConvQuad(dataTypes t1, dataTypes t2) {
         return (t1 != t2 ? dtypeToQuad(t1) + "_TO_" + dtypeToQuad(t2) + "\n" : "");
     }
 };
